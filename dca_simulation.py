5fcf76b8cdbade2f1b6189292421eb3b21e85446
Daniel Solarin <soladan2000@gmail.com>
2025-05-28 04:05:56 -0900

    feat: add last open price to DCA simulation output and update example usage

--- dca_simulation.py
+++ dca_simulation.py
@@ -28,6 +28,7 @@ def simulate_dca(symbol, start_date, end_date, capital_per_interval, interval='1
         total_spent += capital_per_interval
 
     final_price = data['Close'].iloc[-1]
+    last_open_price = data['Open'].iloc[-1]
     current_value = total_quantity * final_price
     pnl = current_value - total_spent
     avg_price = total_spent / total_quantity if total_quantity else 0
@@ -39,6 +40,7 @@ def simulate_dca(symbol, start_date, end_date, capital_per_interval, interval='1
         'quantity_acquired': round(total_quantity, 4),
         'amount_spent': round(total_spent, 2),
         'final_price': round(final_price, 2),
+        'last_open_price': round(last_open_price, 2),
         'current_value': round(current_value, 2),
         'capital_per_interval': capital_per_interval,
         'start_date': start_date,
@@ -55,10 +57,10 @@ def save_dict_to_text_file(data, filename):
 
 if __name__ == "__main__":
     # Example usage
-    symbol = 'AAPL'
+    symbol = 'ANF'
     start_date = '2025-01-01'
     end_date = '2025-05-28'
-    capital_per_interval = 100  # Amount to invest at each interval
+    capital_per_interval = 50  # Amount to invest at each interval
 
     result = simulate_dca(symbol, start_date, end_date, capital_per_interval)
     print(result)
